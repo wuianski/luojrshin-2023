833c65f93cdc43593a23fbe3833bbd8f205a9fa9
wuianski <wuiyeh@gmail.com>
2024-08-28 11:45:36 +0800

    update if there is no image in news list

--- app/[locale]/work/page.jsx
+++ app/[locale]/work/page.jsx
@@ -51,20 +51,21 @@ export default async function Page({ params }) {
                 />
               )}
             </Box>
-
-            <Box sx={{ width: { xs: "100%", md: "50vw" } }}>
-              <Image
-                priority={true}
-                src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${news.image.filename_disk}`}
-                quality={100}
-                width={500}
-                height={500}
-                alt="Picture of the news"
-                style={{ objectFit: "contain", objectPosition: "left" }}
-                sizes="50vw"
-                placeholder={"blurDataURL" in news.image ? "blur" : undefined}
-              />
-            </Box>
+            {news.image ? (
+              <Box sx={{ width: { xs: "100%", md: "50vw" } }}>
+                <Image
+                  priority={true}
+                  src={`${process.env.DIRECTUS_IMAGE_DOMAIN_DO}${news.image.filename_disk}`}
+                  quality={100}
+                  width={500}
+                  height={500}
+                  alt="Picture of the news"
+                  style={{ objectFit: "contain", objectPosition: "left" }}
+                  sizes="50vw"
+                  placeholder={"blurDataURL" in news.image ? "blur" : undefined}
+                />
+              </Box>
+            ) : null}
           </Box>
         ))}
       </Box>
