25b7b81a5a839bbabd11a5df3e300f697e5903ae
wuianski <wuiyeh@gmail.com>
2026-01-25 15:43:42 +0800

    Refactor font exports to include display and adjustFontFallback properties

--- app/[locale]/fonts.ts
+++ app/[locale]/fonts.ts
@@ -1,11 +1,36 @@
-import { Courier_Prime } from 'next/font/google'
-import { Merriweather } from 'next/font/google'
-import { Noto_Serif_TC } from 'next/font/google'
-import { Noto_Sans_TC } from 'next/font/google'
+import { Courier_Prime } from "next/font/google";
+import { Merriweather } from "next/font/google";
+import { Noto_Serif_TC } from "next/font/google";
+import { Noto_Sans_TC } from "next/font/google";
 
 // export const courier_prime700  = Courier_Prime({ subsets: ['latin'], weight: '700' })
-export const courier_prime400  = Courier_Prime({ subsets: ['latin'], weight: '400' })
-export const merriweather300  = Merriweather({ subsets: ['latin'], weight: '300' })
-export const merriweather700  = Merriweather({ subsets: ['latin'], weight: '700' })
-export const noto_serif_tc400  = Noto_Serif_TC({ subsets: ['latin'], weight: '400' })
-export const noto_sans_tc400  = Noto_Sans_TC({ subsets: ['latin'], weight: '400' })
+export const courier_prime400 = Courier_Prime({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  adjustFontFallback: false,
+});
+export const merriweather300 = Merriweather({
+  subsets: ["latin"],
+  weight: "300",
+  display: "swap",
+  adjustFontFallback: false,
+});
+export const merriweather700 = Merriweather({
+  subsets: ["latin"],
+  weight: "700",
+  display: "swap",
+  adjustFontFallback: false,
+});
+export const noto_serif_tc400 = Noto_Serif_TC({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  adjustFontFallback: false,
+});
+export const noto_sans_tc400 = Noto_Sans_TC({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  adjustFontFallback: false,
+});
