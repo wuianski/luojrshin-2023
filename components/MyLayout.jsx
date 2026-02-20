54bc66f974e0ee246ca7fdc26750d7befcb63e1c
wuianski <wuiyeh@gmail.com>
2026-02-21 00:27:22 +0800

    Refactor MyLayout component to conditionally render content based on client-side state

--- components/MyLayout.jsx
+++ components/MyLayout.jsx
@@ -1,31 +1,41 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import { courier_prime400 } from "@/lib/fonts";
 
 export default function MyLayout({ children, locale }) {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <Box
-      sx={{ overflow: "hidden", fontSize: "13px", lineHeight: "1.1em" }}
-      className={
-        locale === "en"
-          ? courier_prime400.className
-          : courier_prime400.className
-      }
-    >
-      <Box
-        sx={{
-          width: { xs: "calc(100vw - 0px)", md: "calc(100vw - 350px)" },
-          marginLeft: { xs: "auto", md: "350px" },
-          marginRight: { xs: "auto", md: "0" },
-          paddingTop: { xs: "60px", md: "0" },
-          backgroundColor: "#fff",
-          height: "calc(100dvh - 60px)",
-          overflowY: "scroll",
-        }}
-      >
-        {children}
-      </Box>
-    </Box>
+    <>
+      {isClient ? (
+        <Box
+          sx={{ overflow: "hidden", fontSize: "13px", lineHeight: "1.1em" }}
+          className={
+            locale === "en"
+              ? courier_prime400.className
+              : courier_prime400.className
+          }
+        >
+          <Box
+            sx={{
+              width: { xs: "calc(100vw - 0px)", md: "calc(100vw - 350px)" },
+              marginLeft: { xs: "auto", md: "350px" },
+              marginRight: { xs: "auto", md: "0" },
+              paddingTop: { xs: "60px", md: "0" },
+              backgroundColor: "#fff",
+              height: "calc(100dvh - 60px)",
+              overflowY: "scroll",
+            }}
+          >
+            {children}
+          </Box>
+        </Box>
+      ) : null}
+    </>
   );
 }
